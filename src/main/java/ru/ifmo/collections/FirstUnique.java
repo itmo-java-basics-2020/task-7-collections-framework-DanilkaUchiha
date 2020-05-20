e42e1866c4e8f1f6203414ccc3fa9d3e7895d441
DanilkaUchiha <44334749+DanilkaUchiha@users.noreply.github.com>
2020-05-20 21:33:17 +0300

    Update FirstUnique.java

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,37 @@
 package ru.ifmo.collections;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+    private TreeSet<Integer> whiteList = new TreeSet<>();
+    private Set<Integer> blackList = new HashSet<>();
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        for (int number : numbers) {
+            add(number);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (whiteList.size() != 0) {
+            return whiteList.first();
+        }
+        return -1;
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (whiteList.contains(value)) {
+            whiteList.remove(value);
+            blackList.add(value);
+        } else if (!multyInt.contains(value)) {
+            whiteList.add(value);
+        }
     }
 }
