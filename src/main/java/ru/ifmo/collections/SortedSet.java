7370b830d711efebacef5e194395d98e1fb04190
DanilkaUchiha <44334749+DanilkaUchiha@users.noreply.github.com>
2020-05-20 21:46:00 +0300

    Update SortedSet.java

--- src/main/java/ru/ifmo/collections/SortedSet.java
+++ src/main/java/ru/ifmo/collections/SortedSet.java
@@ -1,34 +1,45 @@
 package ru.ifmo.collections;
 
+import java.lang.reflect.Array;
 import java.util.*;
+
 /**
  * Represents sorted set of unique values.
  * create() returns a SortedSet instance with natural ordering. (i.e. from smallest to largest in case of integer numbers)
  * from() is used to create a SortedSet instance with given Comparator.
  * Instances of a class can be created using only these two methods above.
- *
+ * <p>
  * This class should not be abstract and should be capable of adding/removing single/multiple elements.
  * It has two more methods: getSorted() and getReversed() which return an array of set contents in forward and reverse order respectively.
- *
+ * <p>
  * NB! This class must have only map(s) as an internal data structure(s).
  *
  * @param <T> set contents type
  */
+public class SortedSet<T> extends AbstractSet<T> {
 
-public  class SortedSet<T> extends AbstractSet<T> {
-    private static final Object EXISTS = new Object();
-    private final Map<T, Object> map;
+    private final transient TreeMap<T, Object> map;
+    private static final Object PRESENT = new Object();
 
-    private SortedSet(TreeMap<T, Object> treeMap) {
-        map = treeMap;
+    private SortedSet() {
+        map = new TreeMap<T, Object>();
+    }
+
+    private SortedSet(Comparator<T> comparator) {
+        map = new TreeMap<T, Object>(comparator);
     }
 
     public static <T> SortedSet<T> create() {
-        return new SortedSet<>(new TreeMap<>());
+        return new SortedSet<T>();
     }
 
     public static <T> SortedSet<T> from(Comparator<T> comparator) {
-        return new SortedSet<>(new TreeMap<>(comparator));
+        return new SortedSet<>(comparator);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
     }
 
     public List<T> getSorted() {
@@ -36,41 +47,13 @@ public  class SortedSet<T> extends AbstractSet<T> {
     }
 
     public List<T> getReversed() {
-        List<T> reversedList = new ArrayList<>(map.keySet());
-        Collections.reverse(reversedList);
-        return reversedList;
+        ArrayList<T> list = new ArrayList<>(map.keySet());
+        Collections.reverse(list);
+        return list;
     }
 
     @Override
-    public boolean add(T t) {
-        return map.put(t, EXISTS) == null;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        boolean isAllAdded = false;
-        for (T it : c) {
-            isAllAdded |= add(it);
-        }
-        return isAllAdded;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return map.remove(o, EXISTS);
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        boolean isAllRemoved = false;
-        for (Object it : c) {
-            isAllRemoved |= remove(it);
-        }
-        return isAllRemoved;
-    }
-
-    @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return map.keySet().iterator();
     }
 
@@ -78,3 +61,14 @@ public  class SortedSet<T> extends AbstractSet<T> {
     public int size() {
         return map.size();
     }
+
+    @Override
+    public boolean add(T t) {
+        return map.put(t, PRESENT) != null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+}
